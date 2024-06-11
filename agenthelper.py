c57930621db6633e188f349308ad09a6037fab1d
dragonfroots <diwas_kc@yahoo.com>
2024-06-10 22:29:17 -0400

    updated file

--- agenthelper.py
+++ agenthelper.py
@@ -1,20 +1,20 @@
 import os
-from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
 from langchain.chains import SequentialChain
+from langchain.chat_models import ChatOpenAI
 
-from my_keys import openapi_key, GOOGLE_CSE_ID, GOOGLE_API_KEY
 
+from dotenv import load_dotenv
+load_dotenv()
+
+os.environ['OPENAI_API_KEY'] = os.getenv('openapi_key')
+os.environ['GOOGLE_CSE_ID'] = os.getenv('GOOGLE_CSE_ID')
+os.environ['GOOGLE_API_KEY'] = os.getenv('GOOGLE_API_KEY')
 
 from langchain.agents import AgentType, initialize_agent, load_tools
 
-
-os.environ['OPENAI_API_KEY'] = openapi_key
-os.environ['GOOGLE_CSE_ID'] = GOOGLE_CSE_ID
-os.environ['GOOGLE_API_KEY'] = GOOGLE_API_KEY
-
-llm = OpenAI(model_name="gpt-3.5-turbo", temperature=0.7)
+llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0.7)
 
 tools = load_tools((["wikipedia", "llm-math"]), llm=llm)
 
@@ -28,15 +28,15 @@ result = agent.run("when was elon musk born")
 print(result)
 
 
-from langchain_community.utilities import GoogleSearchAPIWrapper
-from langchain_core.tools import Tool
-
-search = GoogleSearchAPIWrapper()
-
-search_tool = Tool(
-    name="I'm feeling lucky",
-    description="Search Google for recent results.",
-    func=search.run,
-)
-
-print(search_tool.run("where was obama born"))
+# from langchain_community.utilities import GoogleSearchAPIWrapper
+# from langchain_core.tools import Tool
+#
+# search = GoogleSearchAPIWrapper()
+#
+# search_tool = Tool(
+#     name="I'm feeling lucky",
+#     description="Search Google for recent results.",
+#     func=search.run,
+# )
+#
+# print(search_tool.run("where was obama born"))
